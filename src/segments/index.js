f196ebf4887f181fba96c1371fcd62f8c3250642
Eduardo Ramirez <eramirez@freshsoftwareconcepts.com>
2022-05-12 14:30:13 -0500

    Translates OI, PG30

--- src/segments/index.js
+++ src/segments/index.js
@@ -1,5 +1,6 @@
 const A = require('./A.json');
 const B = require('./B.json');
+const OI = require('./OI.json');
 const PE10 = require('./PE10.json');
 const PE15 = require('./PE15.json');
 const PE90 = require('./PE90.json');
@@ -13,6 +14,7 @@ const PG21 = require('./PG21.json');
 const PG23 = require('./PG23.json');
 const PG26 = require('./PG26.json');
 const PG27 = require('./PG27.json');
+const PG30 = require('./PG30.json');
 const WR1 = require('./WR1.json');
 const Y = require('./Y.json');
 const Z = require('./Z.json');
@@ -20,6 +22,7 @@ const Z = require('./Z.json');
 module.exports = {
   A,
   B,
+  OI,
   PE10,
   PE15,
   PE90,
@@ -33,6 +36,7 @@ module.exports = {
   PG23,
   PG26,
   PG27,
+  PG30,
   WR1,
   Y,
   Z,
